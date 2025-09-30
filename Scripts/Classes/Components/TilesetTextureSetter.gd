b579a757cac3f5abbeb459d724621c2e4e7148c4
CST1229 <68464103+CST1229@users.noreply.github.com>
2025-09-30 21:16:26 +0200

    tileset resolution scaling

--- Scripts/Classes/Components/TilesetTextureSetter.gd
+++ Scripts/Classes/Components/TilesetTextureSetter.gd
@@ -21,6 +21,8 @@ var animation_json: Dictionary
 var animation_frame: int = -1
 var animation_loop: bool
 
+const SCALED_TILE_SHADER = preload("res://Scenes/Parts/scaled_tile.gdshader");
+
 func _ready() -> void:
 	animation_timer.one_shot = true
 	animation_timer.timeout.connect(run_frame)
@@ -42,7 +44,6 @@ func update() -> void:
 			if animation_json.is_empty():
 				animation_loop = false
 				animation_timer.stop()
-				return
 			elif animation_json.has("loop"): # CREATE animations and frames based on the usual SMB1R format
 				animation_loop = animation_json.loop
 				animation_timer.start(1.0 / animation_json.speed)
@@ -58,6 +59,22 @@ func update() -> void:
 						source.set_tile_animation_frames_count(coords, data.frames.size())
 						for i in data.frames.size():
 							source.set_tile_animation_frame_duration(coords, i, data.frames[i].duration)
+			
+			var variations_json = resource_setter.get_variation_json(resource_getter.get_resource(resource_setter.resource_json).data.get("variations", {}))
+			if "resolution_scale" in variations_json and (variations_json.resolution_scale[0] != 1.0 or variations_json.resolution_scale[1] != 1.0):
+				source.use_texture_padding = false;
+				
+				var resolution_scale_vec := Vector2(variations_json.resolution_scale[0], variations_json.resolution_scale[1])
+				var material := ShaderMaterial.new()
+				material.shader = SCALED_TILE_SHADER
+				material.set_shader_parameter("scale", resolution_scale_vec)
+				for tile_index in source.get_tiles_count():
+					var tile := source.get_tile_id(tile_index)
+					for alt_index in source.get_alternative_tiles_count(tile):
+						var alt := source.get_alternative_tile_id(tile, alt_index)
+						var data: TileData = source.get_tile_data(tile, alt)
+						data.material = material
+				
 
 func run_frame() -> void:
 	var frames = animation_json.get("frames", [])
